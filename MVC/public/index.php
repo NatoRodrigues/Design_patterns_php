b17a8781c805ee9a43721cd96142107a19e87bfb
NatoRodrigues <renwise2.0@gmail.com>
2024-04-17 18:45:08 -0300

    PHP-mvc

---
+++ MVC/public/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+    require  "../bootstrap.php";
+
+    //url usada de exemplo:
+    //https://devclass.com.br/curso/edit/12
+
+    $controller = new Controller();
+    $controller = $controller->getController();
+
+    // $method = new Method();
+    // $method->getMethod();
+
+    // $controller->$method();
+
+    // $parameters = new Parameters();
+    // $parameters->getParameters();
+?>
